d788505b2b69730145c39ec362432cb115aec4cc
Harish adi <138894610+Harishadi436@users.noreply.github.com>
2023-11-03 23:50:17 +0530

    Create Day-7.java

---
+++ Day-7.java
@@ -0,0 +1,18 @@
+package com.mock;
+
+public class primenumber1 {
+public static void main(String[] args) {
+int a=5,count=0;
+
+for(int i=1;i<=a;i++) {
+	if(a%i==0) {
+		count++;
+	}
+}if(count==2) {
+	System.out.println("Prime");
+}else {
+	System.out.println("NOt");
+}
+
+}
+}
