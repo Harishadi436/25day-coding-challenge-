5b57cbb7bbfed40628b2ed7fdb20605ba66e48e7
Harish adi <138894610+Harishadi436@users.noreply.github.com>
2023-11-12 19:55:55 +0530

    Create Day-16.java

---
+++ Day-16.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySortWithScanner {
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+
+      
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+       
+        int[] numbers = new int[size];
+
+        
+        System.out.println("Enter the elements of the array:");
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            numbers[i] = scanner.nextInt();
+        }
+
+     
+        scanner.close();
+
+      
+        Arrays.sort(numbers);
+
+        
+        System.out.println("Sorted array in ascending order: " + Arrays.toString(numbers));
+    }
+}
